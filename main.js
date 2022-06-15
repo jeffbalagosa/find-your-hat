033cfed05db4bc0b04a162a314984738b66d9cfd
jeffbalagosa <61676139+jeffbalagosa@users.noreply.github.com>
2022-06-15 07:11:54 -0700

    Restart build of main.js.  Add constructor and .print method.

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
-const prompt = require('prompt-sync')({sigint: true});
+const prompt = require("prompt-sync")({ sigint: true });
 
-const hat = '^';
-const hole = 'O';
-const fieldCharacter = '░';
-const pathCharacter = '*';
+const hat = "^";
+const hole = "O";
+const fieldCharacter = "░";
+const pathCharacter = "*";
+
+class Field {
+  constructor(twoDimenstionalArray) {
+    this._twoDimenstionalArray = twoDimenstionalArray;
+  }
+
+  print() {
+    let arr = this._twoDimenstionalArray;
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+      const e = arr[i];
+      newArr.push(e.join(""));
+    }
+    console.log(newArr);
+  }
+}
+
+// test
+const myField = new Field([
+  ["*", "░", "O"],
+  ["░", "O", "░"],
+  ["░", "^", "░"],
+]);
+
+myField.print();
